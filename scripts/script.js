3f3e8d90b59bf95663e61ab737e5361432a9fc9d
bidduthosen <mdbidduthosen34@gmail.com>
2024-02-15 14:07:18 +0600

    complated the keyboard game

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,96 @@
-function continueGame(){
-    const randomAlphabet = GetARandomAlphabet();
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    
+    // current alphabet element 
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabetText = currentAlphabetElement.innerText;
+    const expectedAlphabet =currentAlphabetText.toLowerCase()
 
+    if(playerPressed === expectedAlphabet){
+        //updated score-------------
+        // 1.get current text convert to number 
+        const currentScore = getTextElementValueById('current-score')
+        const newScore = currentScore + 1;
+        //2. set inner text in current score
+        setTextElementValueById('current-score', newScore);
+
+        // setTextElementValueById('final-score', newScore); // way one--------
+        // get result set score
+        // const resultScore = document.getElementById('final-score'); // way two------------
+        // resultScore.innerText = newScore
+
+        // updated score
+        // const currentScore= document.getElementById('current-score');
+        // const currentScoreText = currentScore.innerText;
+        // const currentScoreValue = parseInt(currentScoreText);
+        // const updatedScore = currentScoreValue + 1;
+        // console.log(updatedScore)
+        // currentScore.innerText = updatedScore;
+
+        removeBackgroundColorById(expectedAlphabet)
+        continueGame()
+    }
+    else{
+        // 1.get current text convert to number 
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        // set inner text in current score
+        setTextElementValueById('current-life', updatedLife);
+        if(updatedLife === 0){
+            gameOver()
+        }
+
+
+        
+        // const currentLife  = document.getElementById('current-life');
+        // const currentLifeText = currentLife.innerText;
+        // const currentLifeValue = parseInt(currentLifeText);
+        // const updateLife = currentLifeValue - 1;
+        // currentLife.innerText = updateLife;
+    }
+}
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
+
+function continueGame(){
+    const alphabet = GetARandomAlphabet();
+    
     // set a random alphabet in show display
-    const currentAlphabet = document.getElementById('current-alphabet');
-    currentAlphabet.innerText = randomAlphabet
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // highlight or background color a current alphabet
+    setBackgroundColorById(alphabet);
+
+    
 }
 
 
 // play
 function play(){
     hideElementById('home-screen');
-    showElementById('playing-section')
+    hideElementById('game-over-section');
+    showElementById('playing-section');
+    // reset life and score
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame()
 } 
+
+// game over
+function gameOver(){
+    showElementById('game-over-section');
+    hideElementById('playing-section');
+    hideElementById('home-screen');
+ 
+    // set last score in game over
+    // const lastScore = document.getElementById('last-score');
+    const currentScore = getElementTextById('current-score')
+    setTextElementValueById('last-score', currentScore)
+
+
+    // remove color by letter 
+    const currentAlphabet = document.getElementById('current-alphabet').innerText;
+    removeBackgroundColorById(currentAlphabet)
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,7 +6,36 @@ function hideElementById(elementId){
 // remove hidden class 
 function showElementById(elementId){
     document.getElementById(elementId).classList.remove('hidden');
-    
+}
+
+// set background color in random alphabet in keyboard
+function setBackgroundColorById(elementId){
+    document.getElementById(elementId).classList.add('bg-orange-400');
+}
+
+// remove background color in random alphabet in keyboard
+function removeBackgroundColorById(elementId){
+    document.getElementById(elementId).classList.remove('bg-orange-400')
+}
+
+// get element value by id
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const elementValue = parseInt(elementText);
+    return elementValue;
+};
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+// get element set text
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
 
 // random alphabet generate return 
